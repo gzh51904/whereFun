478f1a96160dc22e1759fc83d1b921e7e4458e0a
WJD <973951780@qq.com>
2019-07-13 14:06:14 +0800

    修改首页轮播图(wu)

--- src/components/home/banner1.vue
+++ src/components/home/banner1.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="banner1">
-    <el-carousel :interval="5000" arrow="always" indicator-position="string" style="height:17rem;">
-      <el-carousel-item v-for="item in banner1Photo" :key="item.name">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in banner1Photo" :key="item.name">
         <img :src="item.img" alt />
-      </el-carousel-item>
-    </el-carousel>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 <script>
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 export default {
+  name: "carrousel",
   data() {
     return {
-         banner1Photo: [
+      swiperOption: {
+        pagination: {
+          el: ".swiper-pagination"
+        },
+        autoplay: true,
+        effect: "coverflow"
+      },
+      banner1Photo: [
         {
           img: require("../../assets/img/banner_1.jpg"),
           name: 1
@@ -19,10 +29,22 @@ export default {
         {
           img: require("../../assets/img/banner_2.jpg"),
           name: 2
+        },
+        {
+          img: require("../../assets/img/banner_1.jpg"),
+          name: 3
+        },
+        {
+          img: require("../../assets/img/banner_2.jpg"),
+          name: 4
         }
       ]
-    }
+    };
   },
+  components: {
+    swiper,
+    swiperSlide
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -33,8 +55,14 @@ export default {
     height: 17rem;
   }
 }
-.el-carousel__container{
-  height: 17.0rem;
+.el-carousel__container {
+  height: 17rem;
+}
+.swiper-pagination {
+  margin: 0;
+  .swiper-pagination-bullet-active{
+    background: #ff3573 !important;
+  }
 }
 </style>
 
--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     state : {
         name : 'yoga',
-        isShow:true
+        isShow:true,
     },
     mutations: {
 
