1e9d37e6500bd17d46d7601329dd4c264c54dc8d
Infatutaion <971806774@qq.com>
2019-07-16 19:44:53 +0800

    后台管理系统上传

---
+++ wherebms/src/components/Hader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="hader">
+    <h1>后台管理系统</h1>
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        用户名
+        <i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu placement="bottom" slot="dropdown">
+        <el-dropdown-item>个人信息</el-dropdown-item>
+        <el-dropdown-item>退出登录</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
+</template>
+<style lang="scss" scoped>
+#hader {
+  width: 100%;
+  position: relative;
+  line-height: 60px;
+  h1{
+      display: inline;
+      text-align: center;
+      font-size: 26px;
+      font-weight: 900;
+  }
+  .el-dropdown {
+    position: absolute;
+    right: 0;
+  }
+  .el-dropdown-link {
+    cursor: pointer;
+  }
+  .el-icon-arrow-down {
+    font-size: 12px;
+  }
+}
+</style>
+
---
+++ wherebms/src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="mains">
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
