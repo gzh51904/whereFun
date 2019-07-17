85113017ee28132c75dc8f44a309893e9617755e
VinCoFox <1065132225@qq.com>
2019-07-17 22:17:08 +0800

    上线版本

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
+﻿import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import axios from 'axios';
 Vue.prototype.$axios = axios;
-// axios.defaults.baseURL = 'http://47.106.93.239:9527';
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'http://47.106.93.239:9527';
+
 
 
 Vue.config.productionTip = false
